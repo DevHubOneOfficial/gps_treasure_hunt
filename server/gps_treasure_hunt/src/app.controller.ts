4dc50d36d2c9fc05636f55af8811b07d6443bfae
Boyan Ivanov <b.i.ivanov1@gmail.com>
2023-09-10 15:16:14 +0300

    Add image responses

--- server/gps_treasure_hunt/src/app.controller.ts
+++ server/gps_treasure_hunt/src/app.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get, Header, Query, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get, Header, Query, UnauthorizedException, StreamableFile } from '@nestjs/common';
 import { AppService } from './app.service';
 import * as bcrypt from 'bcrypt';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+
 const data = require('../data/data.json');
+
 
 @Controller()
 export class AppController {
@@ -17,7 +21,7 @@ export class AppController {
 
     @Get('coordinates')
     @Header('Cache-Control', 'none')
-    getJson(@Query('accessToken') accessToken: string) {
+    async getJson(@Query('accessToken') accessToken: string) {
         // Validate the access token
         if (!accessToken) {
             throw new UnauthorizedException('Access token is required');
@@ -41,10 +45,35 @@ export class AppController {
         }
 
         if (currentObject !== null) {
-            const { coordinates, objectName, description } = currentObject;
-            return { coordinates, objectName, description };
+            const { coordinates, objectName, imagePath } = currentObject;
+
+            // Read the image file
+            const imageStream = createReadStream(join(__dirname, '..', 'data', imagePath));
+            const imageBase64 = await this.streamToBase64(imageStream);
+
+            // Return the image as part of the JSON response
+            return { coordinates, objectName, image: imageBase64 };
         } else {
             return { message: "No object found for the current date." };
         }
     }
+
+    private async streamToBase64(stream): Promise<string> {
+        return new Promise((resolve, reject) => {
+            const chunks = [];
+            stream.on('data', (chunk) => {
+                chunks.push(chunk);
+            });
+            stream.on('end', () => {
+                const buffer = Buffer.concat(chunks);
+                resolve(buffer.toString('base64'));
+            });
+            stream.on('error', (error) => {
+                reject(error);
+            });
+        });
+    }
+
 }
+
+
